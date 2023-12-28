93f1090305473be8abda780e455176113614f5b2
LaGuize <guilhem.dolet@gmail.com>
2023-12-28 16:46:47 +0100

    Creation des premiers codes pour Monster

---
+++ class_Monster.py
@@ -0,0 +1,43 @@
+class Monster:
+    def __init__(self,name, health, level):
+        self.name = name
+        self.health = health
+        self.level = level
+       
+        if level >= 5:
+            self.has_tiara == True
+        else:
+            self.has_tiara == False
+        
+        
+    def get_attack(self,attack):
+        self.attack = attack
+        return f"You have suffered{self.attack}"
+        
+    def get_damage(self):
+        self.health -= self.attack
+        return f"After having suffered {self.attack} you now have {self.health} points in health"
+    
+    
+class Goblin(Monster): 
+    level = 1
+       
+    def get_attack(self,attack):
+        
+        self.attack == 5      
+    pass    
+ 
+class Minotaur(Monster):
+    level = 5
+    
+    def get_attack(self,attack):
+        self.attack == 15
+    pass    
+
+class Titan(Monster):
+    level = 10
+    
+    def get_attack(self,attack):
+        self.attack == 40
+    pass
+           
