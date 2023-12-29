481fe82064bd4902b9513117cb4f10c4defe4fe5
memo9697 <plancke_guillaume@icloud.com>
2023-12-29 10:14:50 +0100

    modifications monster

--- class_Monster.py
+++ class_Monster.py
@@ -1,26 +1,23 @@
 import random
 
 class Monster:
-    def __init__(self,monster, health, level):
-        
-        self.monster = "Goblin" or "Minotaur" or "Titan"
+    def __init__(self, health, level):
+
+        self.monster = random.choice(["Goblin", "Minotaur", "Titan"])
         self.health = health
         self.level = level
-        self.has_tiara 
         
 
         random.choice(self.monster)
         
-        if self.level == 10:
-            self.has_tiara = True
-             
-        else:
-            self.has_tiara = False
+      
         
         if self.monster == "Titan":
             self.level = 10
             self.attack = 40
+            self.has_tiara = True
             print("Oh tremble in the presence of the notorius Titan")
+    
             
             if self.health == 0:
                 print("The TiTan has left behind the infamous Tiara of Perigourdin ")
@@ -33,11 +30,13 @@ class Monster:
         elif self.monster == "Minotaur":
             self.level = 5
             self.attack = 15
+            self.has_tiara = False
             print("The Minotaur has arisen!Beware of his might!")
         
         else:
             self.level = 1
             self.attack = 5
+            self.has_tiara = False
             print("You are now facing a Goblin")
              
         
@@ -60,5 +59,8 @@ class Monster:
             
             
 
-    
-    
+monster_1 = Monster(100, 1)
+print(monster_1.monster)
+print(monster_1.health)
+print(monster_1.level)
+print(monster_1.has_tiara)
